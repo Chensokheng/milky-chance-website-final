68cfb22dbd6baca9f3b66a237111014ad64c400d
=Chensokheng <=chensokheng18@kit.edu.kh>
2019-05-29 07:40:09 +0700

    testing navbar

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,22 +1,30 @@
 const openMenu = document.querySelector(".hambur");
 const closeBtn = document.querySelector(".closeBtn");
+const closeBtn1 = document.querySelector(".closeBtn1");
 
 const sideMenu = document.querySelector(".side-menu")
+const findUs = document.querySelector(".find-us");
+const socail_container = document.querySelector(".social-container");
 
+findUs.addEventListener("click", () => {
+    socail_container.style.width = "100%";
+})
+
+closeBtn1.addEventListener("click", () => {
+    socail_container.style.width = "0%";
+
+})
 openMenu.addEventListener("click", () => {
     sideMenu.style.width = "100%";
 })
 closeBtn.addEventListener("click", () => {
     sideMenu.style.width = "0%";
+
 })
 
 // display the video
 const video = document.querySelector("video");
 
-window.onload = async function () {
-    try {
-        await video.play()
-    } catch{
-        video.play();
-    }
+window.onload = function () {
+    video.play()
 }  
